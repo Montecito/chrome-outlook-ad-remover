7f52f210a33322b04350de88ff3b07fc328c63d2
Matthias Kapferer <matthias.kapferer@gepardec.com>
2017-10-06 14:09:18 +0200

    Release 1.0

---
+++ outlook_ad_remove.js
@@ -0,0 +1,20 @@
+
+ $(window).on('load', function () {
+    //alert("Window Loaded");
+
+    var ad_removed = false;
+    
+    function check_if_ad_blocker_warning_is_loaded(){
+        if(! ad_removed){
+            if($( "a:contains('Ad-Free Outlook')" ).length == 0){
+                setTimeout(check_if_ad_blocker_warning_is_loaded, 1000);
+            } else {
+                $( "a:contains('Ad-Free Outlook')" ).parent().parent().parent().parent().css("display", "none");
+                ad_removed = true;
+            }
+        }
+    }
+
+    check_if_ad_blocker_warning_is_loaded();
+
+});
