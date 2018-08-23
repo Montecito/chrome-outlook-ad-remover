b213b71b72b9727b3433ba828315e76f1838c058
Montecito <montecito.games@gmail.com>
2018-08-23 14:57:49 +0200

    Adapt to new version

--- outlook_ad_remove.js
+++ outlook_ad_remove.js
@@ -6,10 +6,10 @@
     
     function check_if_ad_blocker_warning_is_loaded(){
         if(! ad_removed){
-            if($( "a:contains('Ad-Free Outlook')" ).length == 0){
+            if($("[aria-label='advertisement']").length == 0){
                 setTimeout(check_if_ad_blocker_warning_is_loaded, 1000);
             } else {
-                $( "a:contains('Ad-Free Outlook')" ).parent().parent().parent().parent().css("display", "none");
+                $("[aria-label='advertisement']").parent().parent().parent().css("display", "none");
                 ad_removed = true;
             }
         }
@@ -18,3 +18,4 @@
     check_if_ad_blocker_warning_is_loaded();
 
 });
+
